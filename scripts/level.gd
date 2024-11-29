ba6054e78d2828e7c2154b4d87ed5a0e65640538
Michal Hrbek <michal@dee.cz>
2024-11-29 21:06:26 +0100

    Continuously spawning rows

--- scripts/level.gd
+++ scripts/level.gd
@@ -13,9 +13,10 @@ var status: GameStatus = GameStatus.IN_PROGRESS:
 		if value != GameStatus.IN_PROGRESS:
 			game_ended.emit(value)
 var walls: Array[Wall] = []
-
 var _tween: Tween
+
 @onready var _lose_trigger: Area2D = $LoseTrigger
+@onready var _row = lvl.start
 
 enum GameStatus {IN_PROGRESS = 0, LOST = 1, WON = 2}
 
@@ -32,25 +33,33 @@ const _slide_duration: float = 0.3
 func _ready():
 	$BottomBorder.reflected.connect(ball_spawner.on_return_ball)
 	# Spawning bricks
-	for i in lvl.height:
-		for j in lvl.width:
-			var index = i*lvl.width+j
-			var wall_res = lvl.walls[index]
-			walls.append(null)
-			if wall_res:
-				var node: Wall = _wall_scenes[wall_res.type].instantiate()
-				if node:
-					node.health = wall_res.health
-					node.level = self
-					_walls_group.add_child(node)
-					node.position += Vector2(Wall.SIZE*(j+0.5),Wall.SIZE*(i+0.5))
-					node.orientation = wall_res.orientation
-					walls[index] = node
-					node.destroyed.connect(_on_wall_destroyed.bind(node,index))
+	for i in _row:
+		_spawn_row(i)
+	_walls_group.position.y += Wall.SIZE*_row
+	
 	color_scheme.on_round_over(self)
 	for i in walls:
 		if i:
 			i.on_round_over()
+
+func _spawn_row(row_index:int):
+	var y = -row_index-1
+	if row_index >= lvl.height or row_index < 0:
+		return
+	for x in lvl.width:
+		var index = row_index*lvl.width+x
+		var wall_res = lvl.walls[index]
+		walls.append(null)
+		if wall_res:
+			var node: Wall = _wall_scenes[wall_res.type].instantiate()
+			if node:
+				node.health = wall_res.health
+				node.level = self
+				_walls_group.add_child(node)
+				node.position += Vector2(Wall.SIZE*(x+0.5),Wall.SIZE*(y+0.5))
+				node.orientation = wall_res.orientation
+				walls[index] = node
+				node.destroyed.connect(_on_wall_destroyed.bind(node,index))
 
 func _check_loss():
 	for i in _lose_trigger.get_overlapping_bodies():
@@ -63,6 +72,8 @@ func _on_wall_destroyed(_wall: Wall, index: int):
 	walls[index] = null
 
 func next_round():
+	_spawn_row(_row)
+	_row += 1
 	if(walls.count(null) == len(walls)):
 		print("YOU WON")
 		status = GameStatus.WON
@@ -72,11 +83,8 @@ func next_round():
 		if i:
 			i.on_round_over()
 	if _tween:
-		if _tween.is_running():
-			_tween.pause()
-			_tween.custom_step(1)
 		_tween.kill()
 	_tween = create_tween()
-	_tween.tween_property(_walls_group, "position", _walls_group.position+Vector2(0,Wall.SIZE), _slide_duration)
+	_tween.tween_property(_walls_group, "position", Vector2(0,_row*Wall.SIZE), _slide_duration)
 	_tween.tween_callback(_check_loss)
 	round_ended.emit()
--- scripts/resources/level_res.gd
+++ scripts/resources/level_res.gd
@@ -1,7 +1,15 @@
 class_name LevelRes extends Resource
 
-# The game area is sized 10x13 walls
 @export var name: String
 @export var width: int = 10
 @export var height: int
+@export var start: int = 0 # How many rows will be visible when the game starts
 @export var walls: Array[WallRes]
+# 12 13 14 15 16 17
+#  6  7  8  9 10 11
+#  0  1  2  3  4  5
+# -- -- -- -- -- -- * start
+
+# The game area is sized 10x12 walls (A wall in the 13th row means losing)
+const GAME_HEIGHT = 12
+const GAME_WIDTH = 10
