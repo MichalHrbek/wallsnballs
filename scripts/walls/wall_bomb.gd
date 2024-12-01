99ebbbb34af34ea34b2b361a9af3d5b8a0b07987
Michal Hrbek <michal@dee.cz>
2024-12-01 11:54:47 +0100

    Bomb chain

--- scripts/walls/wall_bomb.gd
+++ scripts/walls/wall_bomb.gd
@@ -4,9 +4,18 @@ class_name WallBomb extends WallDefault
 @onready var _explosion = preload("res://scenes/effects/explosion.tscn")
 
 func _on_health_depleted():
-	for i in _blast_area.get_overlapping_bodies():
+	var bodies = _blast_area.get_overlapping_bodies()
+	process_mode = PROCESS_MODE_DISABLED
+	
+	# Destroying nearby walls
+	for i in bodies:
 		if i is Wall and i != self:
-			i.destroy()
+			if i is WallDefault:
+				i._on_health_depleted()
+			else:
+				i.destroy()
+	
+	# Spawning the explosion effect
 	var e: GPUParticles2D = _explosion.instantiate()
 	e.finished.connect(e.queue_free)
 	level.effects_node.add_child(e)
@@ -14,6 +23,5 @@ func _on_health_depleted():
 	var m: ParticleProcessMaterial = e.process_material
 	m.color = _polygon.color
 	e.restart()
+	
 	super()
-
-# TODO: Bomb exploding another bomb
