de2e54618ec5e82fb34584bce9cf9d3b5cadf5dc
Michal Hrbek <michal@dee.cz>
2024-12-04 19:24:01 +0100

    Avoid engine bug

--- scripts/ui/level_select.gd
+++ scripts/ui/level_select.gd
@@ -10,6 +10,6 @@ func _ready():
 	for i in levels:
 		_item_list.add_item(i.name)
 
-func _on_item_list_item_selected(index):
+func _on_item_list_item_clicked(index, _at_position, _mouse_button_index):
 	GlobalUiState.selected_level = levels[index]
 	get_tree().change_scene_to_packed(_level_scene)
