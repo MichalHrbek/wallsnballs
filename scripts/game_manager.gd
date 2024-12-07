8ba5b4a122384ef5ec324f26819af7c65caa54ae
Michal Hrbek <michal@dee.cz>
2024-12-07 15:08:55 +0100

    Fixed error when game over dialogs aren't present

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
 @onready var _level = $Level
-@onready var _lose = $LoseDialog
-@onready var _win = $WinDialog
 
 func _ready():
 	_level.game_ended.connect(_on_game_ended)
@@ -11,9 +9,9 @@ func _ready():
 func _on_game_ended(status: Level.GameStatus):
 	#process_mode = PROCESS_MODE_DISABLED
 	if status == Level.GameStatus.WON:
-		_win.visible = true
+		$WinDialog.visible = true
 	elif status == Level.GameStatus.LOST:
-		_lose.visible = true
+		$LoseDialog.visible = true
 
 func change_speed(k: float):
 	Engine.time_scale *= k
