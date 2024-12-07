c28aa6b5e73da46c0005e1d17e3a8b1b01734e71
Michal Hrbek <michal@dee.cz>
2024-12-07 12:09:29 +0100

    Fast forward

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -4,6 +4,13 @@ extends Node2D
 
 func _ready():
 	_level.game_ended.connect(_on_game_ended)
+	_level.round_ended.connect(_on_round_ended)
 
 func _on_game_ended(_status: Level.GameStatus):
 	process_mode = PROCESS_MODE_DISABLED
+
+func change_speed(k: float):
+	Engine.time_scale *= k
+
+func _on_round_ended():
+	Engine.time_scale = 1
