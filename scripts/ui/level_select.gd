e27d683898bf17812d1f3002a724b96d0ff26e99
Michal Hrbek <michal@dee.cz>
2024-12-07 15:07:11 +0100

    Temporary level loading fix

--- scripts/ui/level_select.gd
+++ scripts/ui/level_select.gd
@@ -10,7 +10,8 @@ var _levels: Array[LevelRes] = []
 func _ready():
 	item_clicked.connect(_on_item_list_item_clicked)
 	for i in DirAccess.get_files_at(level_dir):
-		_levels.append(ResourceLoader.load(level_dir+i))
+		_levels.append(load(level_dir+i))
+		#_levels.append(ResourceLoader.load(level_dir+i))
 		add_item(_levels[-1].name)
 
 func _on_item_list_item_clicked(index, _at_position, _mouse_button_index):
